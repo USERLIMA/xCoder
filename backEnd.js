15597ea090b7004e0fcef23afa92372c3fb7c47c
Caio Lima Sousa <caiolgn88@gmail.com>
2022-03-20 16:56:51 -0300

    Ajuste

--- backEnd.js
+++ backEnd.js
@@ -1,3 +1,34 @@
 //Primeira parter - Projeto integrador
 
-console.log('Hello muychaxos')
+//console.log('Hello muychaxos')
+
+const http = require('http');
+const express = require('express');
+
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain'})
+    //res.end("Meu primeiro servidor")
+    switch(req.url) {
+        case "/":
+            res.end("Você está na Home Page!")
+            break
+        case "/contato":
+            res.end("Você está na página de contato")
+            break
+        default:
+            res.end("Você está em nosso servidor!")
+            break
+    }
+}).listen(3000, "localhost");
+
+let backEnd = express()
+
+console.log(backEnd);
+
+//perguntar sobre o 2 parametro:
+
+//backEnd.listen(3000, 'localhost', () => {
+//    console.log('O servidor está rodando')
+//})
+
