17b03448cb1efd11fe0d13f7d38706bf9b9478d0
Caio Lima Sousa <caiolgn88@gmail.com>
2022-03-21 00:06:25 -0300

    .get

--- backEnd.js
+++ backEnd.js
@@ -4,27 +4,28 @@
 
 const http = require('http');
 const express = require('express');
+const backEnd = express();
 
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain'})
+//http.createServer((req, res) => {
+//    res.writeHead(200, { 'Content-Type': 'text/plain'})
     //res.end("Meu primeiro servidor")
-    switch(req.url) {
-        case "/":
-            res.end("Você está na Home Page!")
-            break
-        case "/contato":
-            res.end("Você está na página de contato")
-            break
-        default:
-            res.end("Você está em nosso servidor!")
-            break
-    }
-}).listen(3000, "localhost");
+//    switch(req.url) {
+//        case "/":
+//            res.end("Você está na Home Page!")
+//            break
+//        case "/contato":
+//            res.end("Você está na página de contato")
+//           break
+//        default:
+//            res.end("Você está em nosso servidor!")
+//           break
+//    }
+//}).listen(3000, () => console.log('Servidor reodando na porta 3000'));
 
-let backEnd = express()
+//let backEnd = express()
 
-console.log(backEnd);
+//console.log(backEnd);
 
 //perguntar sobre o 2 parametro:
 
@@ -32,4 +33,7 @@ console.log(backEnd);
 //    console.log('O servidor está rodando')
 //})
 
-//s
+//routing_response
+
+backEnd.get('/', (req, res) => res.send('Olá mundo'))
+backEnd.listen(3000, () => console.log('Servidor rodando na porta 3000'))
